e27913a31d44499173324e7321219fdc4cf2cab6
wnn2019 <807053533@qq.com>
2020-03-14 21:39:54 +0800

    Initial commit

---
+++ src/components/module-system/page/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-table
+    :data="userTable"
+    style="width: 100%">
+    <el-table-column
+      prop="userName"
+      label="用户名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="role"
+      label="角色"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="userStatus"
+      label="状态">
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+    export default {
+        name: "userIndex",
+        data() {
+          return {
+            userTable: [{
+              userStatus: '正常',
+              userName: 'wnn2020',
+              role: '超级管理员'
+            }, {
+              userStatus: '正常',
+              userName: 'wnn2022',
+              role: '超级管理员'
+            },{
+              userStatus: '冻结',
+              userName: 'wnn2023',
+              role: '超级管理员'
+            }, {
+              userStatus: '正常',
+              userName: 'wnn2020',
+              role: '超级管理员'
+            },]
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
