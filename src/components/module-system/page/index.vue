b8abad634c9218d375007932e6692e5c353e457f
wnn2019 <807053533@qq.com>
2020-03-19 17:24:44 +0800

    添加页面完成，处理批量删除逻辑

---
+++ src/components/module-system/page/index.vue
@@ -0,0 +1,171 @@
+<template>
+  <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>系统管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card class="box-card">
+      <div class="tableHeaderToolButtonGroup">
+        <el-button icon="el-icon-plus" size="mini"  @click="handlAdd" ></el-button>
+        <el-button  icon="el-icon-edit" size="mini" circle></el-button>
+        <el-button  icon="el-icon-edit" size="mini"   @click="batchDelete" ></el-button>
+      </div>
+    </el-card>
+    <el-card class="tableCard" >
+      <el-table :data="dataList"  style="width: 100%" border @selection-change="changeCheckBox">
+        <el-table-column type="selection" width="40" prop="userId"> </el-table-column>
+        <el-table-column  prop="username"  label="用户名"  width="180"> </el-table-column>
+        <el-table-column
+          prop="role"
+          label="角色"
+          :formatter="formatterRoles"
+          width="250">
+        </el-table-column>
+        <el-table-column
+          prop="userStatus"
+          label="状态">
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.userStatus"
+              active-color="#13ce66"
+              :active-value= 0
+              :inactive-value=  1
+              @change="handleStatus(scope.row)">
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column
+          fixed="right"
+          label="操作"
+          width="180">
+          <template slot-scope="scope">
+            <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+            <el-button @click="handleClick(scope.row)" type="text" size="small">编辑</el-button>
+            <el-button @click="handleDelete(scope.row)" type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="block">
+        <!-- current-page 当前页数  每页显示数-->
+        <div class="tablePagination">
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="requestParameters.page"
+            :page-size="requestParameters.size"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total">
+          </el-pagination>
+        </div>
+      </div>
+      <!-- 2. v-bind:is绑定当前组件，打开组件  ; 组件之间通信   v-on 父组件监听子组件传递的事件名 ，子组件 this.$emit 传递消息 -->
+      <component v-bind:is="userAdd" ref="addUser"  v-on:refresh-table="doQuery" ></component>
+    </el-card>
+  </div>
+</template>
+<!--主页面板-->
+<script>
+  import {list,remove} from "@/api/base/users"
+  //1.局部组件模块导包
+  import UserAdd from './../components/add'
+    export default {
+      //3.定义引入组件
+      components: {
+        UserAdd,
+      },
+
+      data() {
+        return {
+          userAdd:'UserAdd',
+          dataList: [],
+          multipleSelection: [],
+          total: 0,
+          requestParameters: {
+            page: 1,
+            size: 10,
+          },
+        }
+      },
+      methods: {
+        // 列表查询
+        doQuery(params) {
+          list(this.requestParameters)
+            .then(res => {
+              let resp  = res.data;
+              this.dataList  = resp.data;
+              this.total  = resp.total;
+
+            })
+        },
+        /*
+        *改变每页显示数
+        */
+        handleSizeChange(val) {
+          this.requestParameters.size = val;
+          this.doQuery()
+        },
+        /*
+        *改变当前页码数
+       */
+        handleCurrentChange(val) {
+          this.requestParameters.page = val;
+          this.doQuery()
+        },
+        formatterRoles(row, column) {
+          let roles = row['roles'];
+          let rolesNames = '';
+          if(roles!=undefined){
+            $.each(roles, function (index, val) { //index是数组对象索引，val是对象
+              if (index === 0) {
+                rolesNames = val.roleName;
+              } else {
+                rolesNames = rolesNames + ',' + val.roleName;
+              }
+            });
+          }
+          return rolesNames;
+        },
+        // 添加新员工
+        handlAdd() {
+          this.$refs.addUser.dialogFormVisible=true
+        },
+
+        handleDelete(item) {
+          this.$confirm(
+            `本次操作将删除${item.username}删除后账号将不可恢复，您确认删除吗？`,{
+              type: 'warning'
+            }
+          ).then(() => {
+            remove({ id: item.userId })
+              .then(res => {
+                let resp  = res.data;
+                if (resp.code===200){
+                  this.$message.success('删除成功!');
+                  this.doQuery();
+                }else {
+                  this.$message.success(resp.msg);
+                }
+              })
+          })
+        },
+
+        batchDelete() {
+          // var ids = this.sels.map(item => item.id).join()//获取所有选中行的id组成的字符串，以逗号分隔
+          // this.$message({message: ids,type: "success"});
+          this.$message({message: this.multipleSelection,type: "success"});
+        },
+        changeCheckBox(val){
+          this.multipleSelection = val;
+        },
+      },
+      // 创建完毕状态
+      created: function() {
+        this.doQuery()
+      },
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Index from '../components/Index.vue'
-import UserPage from '../components/module-system/page/UserPage.vue'
+import UserPage from '../components/module-system/page/index.vue'
 
 Vue.use(VueRouter);
 
