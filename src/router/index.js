d407992a0c0b059c6f7c1939b5daf2c48b729689
wnn2019 <807053533@qq.com>
2020-05-16 02:32:17 +0800

    移植登录

--- src/router/index.js
+++ src/router/index.js
@@ -33,4 +33,34 @@ const router = new VueRouter({
   routes
 });
 
+// 全局路由守卫
+router.beforeEach((to, from, next) => {
+  console.log('navigation-guards');
+  // to: Route: 即将要进入的目标 路由对象
+  // from: Route: 当前导航正要离开的路由
+  // next: Function: 一定要调用该方法来 resolve 这个钩子。执行效果依赖 next 方法的调用参数。
+  // 是否登录
+  let isLogin = window.localStorage.getItem("accessToken");
+  // 如果登录了
+  if (isLogin) {
+    // 如果已经登录的话,去登录页，返回首页
+    if (to.path === '/login') {
+      next('/index');
+    }else{
+      // 如果是其他页面的话，直接next()
+      next();
+    }
+  } else {
+    // 如果未登录，跳转登录页
+    if (to.path === '/login') { // 如果是登录页面的话，直接next()
+      next();
+    } else { // 否则 跳转到登录页面
+      next({
+        path: '/login'
+      });
+    }
+  }
+
+});
+
 export default router
--- src/utils/request.js
+++ src/utils/request.js
@@ -7,12 +7,29 @@ import {showLoading,hideLoading} from '@/utils/loadingUtils';
 const instance = axios.create({
   baseURL:  'http://www.tianyu.com', // api的base_url
   timeout: 5000 // request timeout
-})
+});
 
-
+// request interceptor
+instance.interceptors.request.use(
+  config => {
+    // Do something before request is sent
+    let token = window.localStorage.getItem('accessToken');
+    if (token) {
+      // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
+      config.headers['Authorization'] = token
+    }
+    return config
+  },
+  error => {
+    // Do something with request error
+    Message.error('对不起，出错了');
+    console.log(error); // for debug
+    Promise.reject(error);
+  }
+);
 
 export const createAPI = (url, method, data) => {
-  let config = {}
+  let config = {};
   if (method === 'get') {
     config.params = data
   } else {
@@ -49,5 +66,21 @@ export const createFormAPI = (url, method, data) => {
     method,
     ...config
   })
-}
+};
+
+
+// 文件导出
+export const exportFileRequest = (url, params) => {
+  let token = window.localStorage.getItem('accessToken');
+  return axios({
+    method: 'get',
+    url: `http://www.tianyu.com${url}`,
+    params: params,
+    responseType: 'blob',
+    headers: {
+      'Authorization': token
+    }
+  });
+};
+
 
