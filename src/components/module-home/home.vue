271181003faa839de8081bca568fdcaa5cd6da22
wnn <807053533@qq.com>
2020-11-23 18:11:36 +0800

    首页修改2

--- src/components/module-home/home.vue
+++ src/components/module-home/home.vue
@@ -1,7 +1,7 @@
 <!--后台首页-->
 <template>
 
-    <el-col class="tableHeaderToolButtonGroup" :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+    <el-col class="homepage" :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
       <!--   第一行4卡片内容   -->
       <el-row :gutter="12">
         <el-col :span="6">
@@ -47,18 +47,41 @@
         </el-col>
         <el-col :span="6">
           <el-card shadow="always">
-            <p style="font-size:12px;font-family:'Microsoft YaHei'">
-              用户活跃数
+            <p style="font-size:13px;font-family:'Microsoft YaHei';color: #999999">
+              今日访问人数
             </p>
-            <p style="font-size:20px;font-family:'Microsoft YaHei'">
-              总用户数
+            <p style="font-size:28px;font-family:'Microsoft YaHei'">
+              <i class="el-icon-thumb"></i> 1,800
+            </p>
+            <p style="font-size:13px;font-family:'Microsoft YaHei'">
+              周同比12%  <i class="el-icon-arrow-up"></i> 日同比11% <i class="el-icon-arrow-down"></i>
+            </p>
+            <el-divider></el-divider>
+            <p style="font-size:13px;font-family:'Microsoft YaHei';float:left;">
+              总访问人数
+            </p>
+            <p style="font-size:13px;font-family:'Microsoft YaHei';float:left;">
+              <i class="el-icon-thumb"></i> 2,795,852
             </p>
           </el-card>
         </el-col>
         <el-col :span="6">
           <el-card shadow="always">
-            <p style="font-size:12px;font-family:'Microsoft YaHei'">
+            <p style="font-size:13px;font-family:'Microsoft YaHei';color: #999999">
               待办事项
+            </p>
+            <p style="font-size:28px;font-family:'Microsoft YaHei'">
+              <i class="el-icon-edit"></i> 3
+            </p>
+            <p style="font-size:13px;font-family:'Microsoft YaHei'">
+              正在处理 2 <i class="el-icon-document-copy"></i> 未解决 1 <i class="el-icon-close"></i>
+            </p>
+            <el-divider></el-divider>
+            <p style="font-size:13px;font-family:'Microsoft YaHei';float:left;">
+              总解决数
+            </p>
+            <p style="font-size:13px;font-family:'Microsoft YaHei';float:left;">
+              <i class="el-icon-check"></i> 8
             </p>
           </el-card>
         </el-col>
@@ -67,12 +90,14 @@
       <!--   第一行 日期内容   -->
       <el-card  shadow="always" style="margin-top: 14px">
         <el-row :gutter="10">
+          <el-col :span="12">
+            <div class="grid-content bg-purple">
+             <div id="main" style="width: 800px;height: 400px;"></div>
+            </div>
+          </el-col>
           <el-col :span="12"><div class="grid-content bg-purple">
-            <div id="main" style="width: 600px;height: 400px;"></div>
-          </div></el-col>
-          <el-col :span="12"><div class="grid-content bg-purple">
-            <el-calendar v-model="value">
-            </el-calendar>
+<!--            <el-calendar v-model="value">-->
+<!--            </el-calendar>-->
           </div></el-col>
         </el-row>
       </el-card>
@@ -90,55 +115,48 @@
           return {
             value: new Date(),
             charts: '',
-            opinion:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎'],
-            opinionData:[
-              {value:335, name:'直接访问'},
-              {value:310, name:'邮件营销'},
-              {value:234, name:'联盟广告'},
-              {value:135, name:'视频广告'},
-              {value:1548, name:'搜索引擎'}
-            ],
-
           }
         },
         methods:{
           drawPie(id){
             this.charts = echarts.init(document.getElementById(id))
             this.charts.setOption({
+              title: {
+                text: '最近一周访问人数',
+              },
+              color: ['#3398DB'],
               tooltip: {
-                trigger: 'item',
-                formatter: '{a}<br/>{b}:{c} ({d}%)'
+                trigger: 'axis',
+                axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                  type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                }
               },
-              legend: {
-                orient: 'vertical',
-                x: 'left',
-                data:this.opinion
+              grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
               },
+              xAxis: [
+                {
+                  type: 'category',
+                  data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                  axisTick: {
+                    alignWithLabel: true
+                  }
+                }
+              ],
+              yAxis: [
+                {
+                  type: 'value'
+                }
+              ],
               series: [
                 {
-                  name:'访问来源',
-                  type:'pie',
-                  radius:['50%','70%'],
-                  avoidLabelOverlap: false,
-                  label: {
-                    normal: {
-                      show: false,
-                      position: 'center'
-                    },
-                    emphasis: {
-                      show: true,
-                      textStyle: {
-                        fontSize: '30',
-                        fontWeight: 'blod'
-                      }
-                    }
-                  },
-                  labelLine: {
-                    normal: {
-                      show: false
-                    }
-                  },
-                  data:this.opinionData
+                  name: '人数',
+                  type: 'bar',
+                  barWidth: '60%',
+                  data: [95, 52, 200, 334, 59, 330, 220]
                 }
               ]
             })
